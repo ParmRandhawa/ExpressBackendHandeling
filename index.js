d182c8d98263717c10756e971fcb0eb464a8b199
Your Name <you@example.com>
2020-07-19 12:17:39 -0400

    Express Forms

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
 
 const app = express();
+const upload = multer();
 const routes = require('./routes/routes.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(upload.array());
 
 app.set('view engine', 'pug');
 app.set('views', './views');
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('first_view')
+    res.render('form');
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('form submitted');
 });
 
 module.exports = router;
