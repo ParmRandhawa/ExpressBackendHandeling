eb0ab65433917bf61ed960e775473ca940b5c579
Your Name <you@example.com>
2020-07-28 16:40:30 -0400

    user handeling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
+const session = require('express-session');
 
 const app = express();
 const upload = multer();
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(upload.array());
+app.use(session({secret: 'this is a secret key'}))
 
 app.set('view engine', 'pug');
 app.set('views', './views');
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,71 @@
 const express = require('express');
 const router = express.Router();
 
+const Users = [];
+
 router.get('/', (req, res) => {
     res.render('form');
 });
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('form submitted');
+    if (!req.body.userName || !req.body.password) {
+        res.status('400');
+        res.send('invalid  details!');
+    } else {
+        Users.filter((user) => {
+            if (user.userName == req.body.userName) {
+                res.render('/', {
+                    message: 'The user already exists'
+                })
+            }
+        });
+
+        const newUser = {userName: req.body.userName, password: req.body.password}
+
+        Users.push(newUser);
+        req.session.user = newUser;
+
+        res.redirect('/user_page');
+    }
+});
+
+function userLoggedIn(req, res, next) {
+    if (req.session.user)
+        next();
+    else {
+        const err = new Error('Not Logged in!');
+        console.log(req.session.user);
+        next(err);
+    }
+}
+
+router.get('/user_page', userLoggedIn, (req, res) => {
+    res.render('user_page', {userName: req.session.user.userName});
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/login', (req, res) => {
+    if (!req.body.userName || !req.body.password) {
+        res.render('login', {message: 'enter username and password'})
+    } else {
+        Users.filter((user) => {
+            if (user.userName === req.body.userName && user.password === req.body.password) {
+                req.session.user = user;
+                res.redirect('/user_Page');
+            } 
+        });
+        res.render('login', {message: 'Invalid Credentials'});
+    }
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        console.log('user logged out');
+    })
+    res.redirect('/login')
 });
 
 module.exports = router;
